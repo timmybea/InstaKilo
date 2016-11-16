418ecfc3cbaa38ac034d7ea64d30953f6074e678
Tim Beals <tim.beals@gmail.com>
2016-11-16 12:25:54 -0800

    initial setup with photos

---
+++ InstaKilo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaKilo
+//
+//  Created by Tim Beals on 2016-11-16.
+//  Copyright © 2016 Tim Beals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
